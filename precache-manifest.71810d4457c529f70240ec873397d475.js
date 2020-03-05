d32660331c764c7b76cccefb20064001d6e7b0d7
cap.Antonio <cheremnykh.anton@gmail.com>
2020-03-05 21:21:08 +0100

    Updates

---
+++ precache-manifest.71810d4457c529f70240ec873397d475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299df3b55b12e0f78e437e97f029ba46",
+    "url": "/star-wars-guide---react-app.git/index.html"
+  },
+  {
+    "revision": "b0b6fa86309589916ee1",
+    "url": "/star-wars-guide---react-app.git/static/css/main.3690db49.chunk.css"
+  },
+  {
+    "revision": "1eb77c82c4e4cfb905b9",
+    "url": "/star-wars-guide---react-app.git/static/js/2.619f0317.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/star-wars-guide---react-app.git/static/js/2.619f0317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b6fa86309589916ee1",
+    "url": "/star-wars-guide---react-app.git/static/js/main.4d0fc16b.chunk.js"
+  },
+  {
+    "revision": "e92c68a74294539966f6",
+    "url": "/star-wars-guide---react-app.git/static/js/runtime-main.441241a2.js"
+  }
+]);
