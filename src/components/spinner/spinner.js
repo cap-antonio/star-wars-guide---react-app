15142c06a078ca9673a68f3e8cec40a6008de85c
cap.Antonio <cheremnykh.anton@gmail.com>
2020-03-12 22:07:53 +0100

    Spinner created

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-9pf1vwfa65t"><div className="ldio-x51gv0fvdua">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div></div>
+
+    )
+}
+
+export default Spinner
